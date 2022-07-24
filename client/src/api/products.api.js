a94b0efa9a542a78032fc9acbb8072a475ae12f8
Laura Gonzalez <laurasgm@Lauras-MacBook-Pro.local>
2022-07-23 21:22:13 -0500

    deploying app with docker compose & ec2 aws

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getProductsRequest = async (search) =>{
-    return await axios.get("http://localhost:4000/products", {params:  {search: search}})
+    return await axios.get(process.env.REACT_APP_API, {params:  {search: search}})
 }
 
 export const getProductByIdRequest = async (id) =>{
-    return await axios.get(`http://localhost:4000/products/${id}`)
+    return await axios.get(`${process.env.REACT_APP_API}/${id}`)
 }
 
 export const getProductDescriptionByIdRequest = async (id) =>{
-    return await axios.get(`http://localhost:4000/products/${id}/description`)
+    return await axios.get(`${process.env.REACT_APP_API}/${id}/description`)
 }
